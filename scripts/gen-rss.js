945fcdca214e8fdac1ef5860c4f16e7737ef0f4d
JD Alewine <105988369+mysticbooze@users.noreply.github.com>
2022-08-03 08:41:25 -0400

    Update gen-rss.js
    Signed-off-by: JD Alewine <105988369+mysticbooze@users.noreply.github.com>

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Them Bites',
+    site_url: 'https://thembites.com',
+    feed_url: 'https://thembites.com/feed/'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
