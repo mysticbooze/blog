38a2eaf26ebc8ee3acc68d137268dbe1cc520a05
JD Alewine <105988369+mysticbooze@users.noreply.github.com>
2022-08-03 08:32:49 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,8 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> Them Bites
+      <a href="https://thembites.com/feed/">RSS</a>
       <style jsx>{`
         a {
           float: right;
